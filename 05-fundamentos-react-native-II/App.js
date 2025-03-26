6054e026abb0109deb7fb08ca13c65e918230217
gustavoclay <gustavoclay@gmail.com>
2025-03-26 20:36:00 -0300

    aula 05 fundamentos II

---
+++ 05-fundamentos-react-native-II/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import PrimeiroComponente from './componentes/PrimeiroComponente';
+import JavascriptComponente from './componentes/JavascriptComponente';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+
+      <JavascriptComponente />
+ 
+      
+
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
